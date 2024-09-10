fffbd37e5c6ed129a89fd75a98859e0d58256af3
hussain7abbas <hussain.abduljaleel1905@coeng.uobaghdad.edu.iq>
2024-09-10 20:06:46 +0300

    **[tailwind]:** complete

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,15 @@
 const AboutPage = () => {
   return (
-    <div className='flex flex-col bg-black text-blue-400'>
-      <h3>A Little Bit About Me</h3>
-      <p>Full Stack Developer and Team Leader with 5+ years of experience in software development and project management. Skilled in leading teams to deliver high-quality projects, enhancing codebases, Proficient in frontend (React.js) and backend (Node.js) development, and experienced in mentoring developers and UX/UI designers. Strong in Agile methodologies, delivering solutions that drive business success.</p>
+    <div className='flex flex-row-reverse max-lg:flex-col'>
+      <img
+        src='/personal_photo.jpg'
+        className='lg:flex-shrink-[1] self-center h-[40rem] object-top rounded-lg object-cover max-lg:w-[15rem] max-lg:h-[15rem] max-lg:rounded-full'
+        alt='Personal Photo'
+      />
+      <div className='lg:flex-shrink-[2] lg:me-2 max-lg:mt-2'>
+        <h3>A Little Bit About Me</h3>
+        <p>Full Stack Developer and Team Leader with 5+ years of experience in software development and project management. Skilled in leading teams to deliver high-quality projects, enhancing codebases, Proficient in frontend (React.js) and backend (Node.js) development, and experienced in mentoring developers and UX/UI designers. Strong in Agile methodologies, delivering solutions that drive business success.</p>
+      </div>
     </div>
   );
 };
