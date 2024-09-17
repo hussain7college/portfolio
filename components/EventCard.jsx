22c6fabcbb6aa1a41c7e352ead0b9556d373d191
Hussain Abbas <hussain.abduljaleel1905@coeng.uobaghdad.edu.iq>
2024-09-17 13:05:37 +0300

    **[projects]:** complete

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -1,10 +1,9 @@
-import Image from 'next/image';
 import styles from '/styles/EventCard.module.css';
 
 const EventCard = ({ event }) => {
   return (
     <div className={styles.card}>
-      <Image src={event.image} height={300} width={600} alt={event.name} />
+      <img src={event.image} className={styles.image} alt={event.name} />
       <div className={styles.content}>
         <h3>{event.name}</h3>
         <p>{event.description}</p>
--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,10 +1,9 @@
-import Image from 'next/image';
 import styles from '/styles/ProjectCard.module.css';
 
 const ProjectCard = ({ project }) => {
   return (
     <div className={styles.card}>
-      <Image src={project.image} height={300} width={600} alt={project.name} />
+      <img src={project.image} className={styles.image} alt={project.name} />
       <div className={styles.content}>
         <h3>{project.name}</h3>
         <p>{project.description}</p>
